f7e3a384e50ca87529953d0aa8da7a17b3ccc785
Benedikt Terhechte <terhechte@gmail.com>
2023-04-04 17:12:57 +0200

    window focus hack

--- examples/window_focus.rs
+++ examples/window_focus.rs
@@ -1,42 +1,43 @@
+#![allow(non_snake_case)]
 use dioxus::prelude::*;
-use dioxus_desktop::tao::event::WindowEvent;
-use dioxus_desktop::use_wry_event_handler;
-use dioxus_desktop::wry::application::event::Event as WryEvent;
 
 fn main() {
     dioxus_desktop::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
-    let focused = use_state(cx, || false);
-
-    use_wry_event_handler(cx, {
-        to_owned![focused];
-        move |event, _| {
-            if let WryEvent::WindowEvent {
-                event: WindowEvent::Focused(new_focused),
-                ..
-            } = event
-            {
-                focused.set(*new_focused);
-            }
-        }
-    });
+    let value = use_state(cx, || false);
 
     cx.render(rsx! {
-        div{
+        div {
             width: "100%",
             height: "100%",
             display: "flex",
             flex_direction: "column",
             align_items: "center",
             {
-                if *focused.get() {
-                    "This window is focused!"
+                if *value.get() {
+                    rsx!(Example {
+                        value: 10
+                    })
                 } else {
-                    "This window is not focused!"
+                    rsx!(Example {
+                        value: 20
+                    })
                 }
+            }
+            button {
+                onclick: move |_| value.set(!*value.get()),
+                "Toggle (current is {value})"
             }
         }
     })
+}
+
+#[inline_props]
+fn Example(cx: Scope, value: usize) -> Element {
+    let state = use_state(cx, || *value);
+    cx.render(rsx!(h3 {
+        "Value is {state}"
+    }))
 }
